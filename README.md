62583589f7df72287d1565c35f871e03b2a003d8
Segun Samuel <shegs-a@users.noreply.github.com>
2022-05-21 20:50:43 +0000

    - Added whitespace removal to remove whitespaces from the user's input. - Parsed the user input to integer so mathematical operations can be carried out on the operands.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS-Calculator
 My attempt at a Javascript calculator on NodeJS
+
+The calculator aims to capture input from the user and perform the required operations based on the user's input.
--- index.js
+++ index.js
@@ -32,69 +32,70 @@ function calculator(){
     /*
         This section checks for the operator inputted by the user.
         Gets the left and right operand, removes the spaces in the left and right operand.
+        The replaceAll function is used to remove the spaces in any of the operands by replacing the space with no space.
+
+        parseInt converts the string to integers
+        
 
     */
     if(input.includes('+')){
-      leftOperand = parseInt(input.split("+").shift());
-      rightOperand = parseInt(input.split("+").pop());
+
+      //splits the input string at the + operator and stores the string to the left of the operator
+      leftOperand = input.split('+').shift(); 
+      leftOperand = parseInt(leftOperand.replaceAll(' ','')); 
+
+      //splits the input string at the + operator and stores the string to the right of the operator
+      rightOperand = input.split('+').pop();
+      rightOperand = parseInt(rightOperand.replaceAll(' ','')); 
 
       add(leftOperand, rightOperand);
     }
 
     else if(input.includes('-')){
-      leftOperand = parseInt(input.split("-").shift());
-      rightOperand = parseInt(input.split("-").pop());
+      leftOperand = parseInt((input.split('-').shift()).replaceAll(' ',''));
+      rightOperand = parseInt((input.split('-').pop()).replaceAll(' ',''));
 
       subtract(leftOperand, rightOperand);
+    }
 
-    
+    else if(input.includes('*')){
+      leftOperand = parseInt((input.split('*').shift()).replaceAll(' ',''));
+      rightOperand = parseInt((input.split('*').pop()).replaceAll(' ',''));
+
+      product(leftOperand, rightOperand);
+    }
+
+    else if(input.includes('/')){
+      leftOperand = parseInt((input.split('/').shift()).replaceAll(' ',''));
+      rightOperand = parseInt((input.split('/').pop()).replaceAll(' ',''));
+
+      divide(leftOperand, rightOperand);
+    }
+
+    else if(input.includes('%')){
+      leftOperand = parseInt((input.split('%').shift()).replaceAll(' ',''));
+      rightOperand = parseInt((input.split('%').pop()).replaceAll(' ',''));
+
+      modulo(leftOperand, rightOperand);
+    }
+
+    else
+      console.log("Please include a valid operator in your input");
   }
   
   //declare the console prompt to capture user input
   const name = prompt("Welcome, please enter your name ");
   console.log('Hi ',name, '!');
+
+  console.log("\nPlease note that you can only perform addition, subtraction, multiplication, division and modular arithmetic with this calculator!");
   
-  const userInput = prompt("Now input your calculations in this format: 2+2, 4-3, 5*3 etc. and press enter ");
+  const userInput = prompt("\nNow input your calculations in this format: 2+2, 4-3, 5*3 etc. and press enter ");
 
   operation(userInput);
 
   console.log("Press Run to restart the calculator");
 }
-  /*function operation(num1, num2, operator){
-    if (operator === '+'){
-      console.log(num1 + num2);
-    }
-    else if(operator === '-'){
-      subtractNums(num1, num2);
-    }
-    else if(operator === '*'){
-      console.log(num1*num2);
-    }
-    else if(operator === '/'){
-      console.log(num1/num2);
-    }
-    else if(operator === '%'){
-      console.log(num1%num2);
-    }
-    else console.log("Please use a valid operator!");
-  }
 
-  //declare the console prompt to capture user input
-  const name = prompt("Welcome, please enter your name ");
-  console.log('Hi ',name, '!');
-  
-  const userInput = prompt("Now input your calculations in this format: 2+2, 4-3, 5*3 etc. and press enter ");
-
-  
-  
-  /*const calc = userInput.split("");
-  
-  if (calc.length > 3)
-    console.log("Please enter your commands in this format: 2+2, 4-3, 5*3 without spaces and press enter ");
-  else
-    operation(parseInt(calc[0]), parseInt(calc[2]), calc[1]);*/
-  
-  
-
+//the code starts here.
 calculator();
 //End
